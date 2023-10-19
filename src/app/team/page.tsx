9460b811791e955d8f5fa49584187a0cf9673832
CKAY-9 <foxtrotcyanide@gmail.com>
2023-10-18 22:28:42 -0700

    Add personal bio for Danny and Parham

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -28,7 +28,8 @@ const TeamPage = () => {
                         </div>
                         <div className={style.member}>
                             <h3>Parham Khoshsiar</h3>
-                            <span>Awaiting bio.</span>
+                            <span>I am Parham Khoshsiar, born in Tehran, Iran in 2006. When I was just 7 years old, my family embarked on a life-changing journey, relocating to the nation of Canada. Today, I am deeply passionate about both sports and academics, and trying to make my family proud.
+My interest in coding was ignited during my 8th-grade year, where I was introduced to the fundamentals of Python and made a small, but impactful, coding project. Over the years, I have been on a steady path of growth and refinement in my programming skills. Although I am by no means an expert in the field, I am steadily progressing and working toward my ultimate goal of securing a career in software development.</span>
                         </div>
                     </div>
                 </section>
@@ -53,7 +54,7 @@ const TeamPage = () => {
                     <div className={style.members}>
                         <div className={style.member}>
                             <h3>Danny Watts</h3>
-                            <span>Awaiting bio.</span>
+                            <span>I enjoy spending my down time on my computer and chatting with friends. I have a passion for computer science and programming. Marketing being one of my specialties allows for me to support the club outside of just programming and leadership.  LCFC FOR LIFE!.</span>
                         </div>
                         <div className={style.member}>
                             <h3>Pran Tito</h3>
